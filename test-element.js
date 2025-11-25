bfbc82260ac06e5b038187e3061d6ea1b7f18097
bcooperwv <bryancooper@gmail.com>
2025-11-25 11:56:32 -0500

    Update test-element.js

--- test-element.js
+++ test-element.js
@@ -1,8 +1,8 @@
 javascript
-     class TestElement extends HTMLElement {
-       connectedCallback() {
-         console.log('Test Element Loaded');
-         this.innerHTML = 'Test Element Loaded';
-       }
-     }
-     customElements.define('test-element', TestElement);
+class TestElement extends HTMLElement {
+  connectedCallback() {
+    console.log('Test Element Loaded');
+    this.innerHTML = 'Test Element Loaded';
+  }
+}
+customElements.define('test-element', TestElement);
